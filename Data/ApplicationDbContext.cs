e3f93bb953000bbd108563c58df274e6d8295306
Daniel Benson <forktogether.dev@gmail.com>
2025-09-19 13:39:08 +0100

    Set up database connection and model

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using FootballTeamManager.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Reflection.Emit;
+
+namespace FootballTeamManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Player> Players { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Ensure jersey numbers are unique
+            modelBuilder.Entity<Player>()
+                .HasIndex(p => p.JerseyNumber)
+                .IsUnique();
+
+            modelBuilder.Entity<Player>()
+                .Property(p => p.Position)
+                .HasConversion<string>();
+        }
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FootballTeamManager.Models
+{
+    public enum Position
+    {
+        Goalkeeper,
+        Defender,
+        Midfielder,
+        Forward
+    }
+
+
+    public class Player
+    {
+        [Key]
+        public int PlayerId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string PlayerName { get; set; } = string.Empty;
+
+        [Required]
+        public Position Position { get; set; }
+        
+        [Required]
+        public int JerseyNumber { get; set; }
+        
+        [Required]
+        public int GoalsScored { get; set; } = 0;
+        
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        
+        public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
